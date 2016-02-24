50bb25caaab667d6ae593daa20237a5d6ed1b48f
egold555 <eric@golde.org>
2016-02-23 19:11:29 -0800

    Initial Commit
    Initial Commit of the program

---
+++ illuminati/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace illuminati
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(95, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(936, 456);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1155, 575);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ illuminati/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Net;
+
+namespace illuminati
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //do everything in order
+            playMusic();
+            pictureBox1.Image = illuminati.Properties.Resources.scp;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+       
+
+        private void playMusic()
+        {
+            try
+            {
+                SoundPlayer sndplayr = new
+                         SoundPlayer(illuminati.Properties.Resources.music);
+                sndplayr.PlayLooping(); //loop the xfiles theme song
+                
+
+            }
+            catch (Exception ex)
+            {
+                //for some reason if a error occures it puts it on the screen (this shouldent happen)
+                MessageBox.Show(ex.Message + ": " + ex.StackTrace.ToString(),
+                               "Error");
+                return;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //exit the app when you push a key
+            //crashes vhost
+            //wonder why..
+            Environment.Exit(0);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            String country, state, city, lon, lat, zip, ip;
+            WebClient client = new WebClient();
+
+            try
+            {
+                //get the users ip
+                ip = client.DownloadString("https://api.ipify.org");
+
+                //download evrtything needed
+                country = client.DownloadString("http://ip-api.com/line/" + ip + "?fields=country");
+                state = client.DownloadString("http://ip-api.com/line/" + ip + "?fields=regionName");
+                city = client.DownloadString("http://ip-api.com/line/" + ip + "?fields=city");
+                lon = client.DownloadString("http://ip-api.com/line/" + ip + "?fields=lon");
+                lat = client.DownloadString("http://ip-api.com/line/" + ip + "?fields=lat");
+                zip = client.DownloadString("http://ip-api.com/line/" + ip + "?fields=zip");
+            }
+            catch (Exception ex)
+            {
+                //if user isnt connected to internet cancel the speech
+                // MessageBox.Show(ex.Message + ": " + ex.StackTrace.ToString(),  "Error");//optional error
+
+                return;
+            }
+
+
+            // Initialize a new instance of the SpeechSynthesizer
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output. 
+            synth.SetOutputToDefaultAudioDevice();
+
+            //Set the volume to 100 because why not
+            synth.Volume = 100;
+
+            // Speak a string
+            synth.Speak("We are the illuminati. You have entered the illuminati.");
+            pictureBox1.Image = illuminati.Properties.Resources.eye_hand_horus_illuminati_indie_Favim_com_236960;
+            synth.Speak("Now your life is in our hands.");
+            pictureBox1.Image = illuminati.Properties.Resources.earth_clipart_black_and_white_aiek4Kzi4;
+            synth.Speak("Your current location is" + city + state + country + "zipcode" + zip + "at approximately" + lon + lat + "degrees.");
+            synth.Speak("Illuminati is watching.");
+            pictureBox1.Image = illuminati.Properties.Resources.scp;
+        }
+
+    
+    }
+}
